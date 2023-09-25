f3b4c107d9677385ce8f611d3e019b39edf627a2
lucidsoft <aromal.lilly@gmail.com>
2023-09-25 16:49:44 -0400

    adding rosviewer component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from "./store";
 import Cart from "./component/Cart";
+import MyRosviewer from './component/MyRosviewer';
 
 function App() {
   return (
     <Provider store={store}>
       <Cart />
+      <MyRosviewer />
     </Provider>
   );
 }
---
+++ src/component/MyRosviewer.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import ROS3D from 'ros3d';
+import ROSLIB from 'roslib'
+//import './css/general.css';
+
+class MyRosviewer extends Component {
+
+    render() {
+
+        let ros = new ROSLIB.Ros({
+          url : 'ws://localhost:9090'
+        });
+
+        console.log(ros);
+
+        let viewer = new ROS3D.Viewer({
+          divID : 'urdf',
+          width : 800,
+          height : 600,
+          antialias : true
+        });
+
+        viewer.addObject(new ROS3D.Grid());
+
+        let tfClient = new ROSLIB.TFClient({
+          ros : ros,
+          angularThres : 0.01,
+          transThres : 0.01,
+          rate : 10.0
+        });
+
+        let urdfClient = new ROS3D.UrdfClient({
+          ros : ros,
+          tfClient : tfClient,
+          path : 'http://resources.robotwebtools.org/',
+          rootObject : viewer.scene,
+          loader : ROS3D.COLLADA_LOADER_2
+        });
+
+        return (
+            <div id="urdf"></div>
+        );
+    }
+}
+
+export default MyRosviewer;
